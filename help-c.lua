089ea9aa439be77d0c34d4aadf87110248bcc95f
Jordan3041 <102299689+Jordan3041@users.noreply.github.com>
2022-03-24 17:55:44 -0600

    Add files via upload

--- help-c.lua
+++ help-c.lua
@@ -1,7 +1,8 @@
 RegisterCommand("help", function()
-    msg("Join the Help Channel on our Discord for Help! and Ping the Support Role")
-    msg("Server Discord: https://discord.gg/G6MXRY7m9C")
+    msg("YOUR TEXT HERE")
+    msg("YOUR TEXT HERE")
 end, false)    
+
 
 function msg(text)
     TriggerEvent("chatMessage", {Server}, {255,0,0}, text)
