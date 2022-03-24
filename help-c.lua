d4ce134ac013082f62b11b6f64601b698e3dc0f8
Jordan3041 <102299689+Jordan3041@users.noreply.github.com>
2022-03-24 17:50:33 -0600

    Add files via upload

--- help-c.lua
+++ help-c.lua
@@ -1,4 +1,5 @@
 RegisterCommand("help", function()
+    msg("Join the Help Channel on our Discord for Help! and Ping the Support Role")
     msg("Server Discord: https://discord.gg/G6MXRY7m9C")
 end, false)    
 
